30aab811740b6d0576fd2616d35ffaf901a00aba
Dan Colish <dcolish@gmail.com>
2013-02-21 08:02:37 -0800

    hadoop presentation

--- hadoop-hears-a-who/sample_emr.py
+++ hadoop-hears-a-who/sample_emr.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 
 import boto
 from boto.emr.instance_group import InstanceGroup
@@ -9,12 +10,21 @@ conn = boto.connect_emr()
 
 instance_groups = [
     InstanceGroup(1, 'MASTER', 'm1.small', 'SPOT', 'master@0.10', '0.10'),
-    InstanceGroup(2, 'CORE', 'm1.medium', 'SPOT', 'master@0.10', '0.10'),
+    InstanceGroup(2, 'CORE', 'm1.small', 'SPOT', 'master@0.10', '0.10'),
 ]
 
 pig_file = 's3://elasticmapreduce/samples/pig-apache/do-reports2.pig'
 INPUT = 's3://elasticmapreduce/samples/pig-apache/input/'
-OUTPUT = 's3://org.unencrypted.emr.output/apache_sample/%s' % datetime.datetime.utcnow().isoformat()
+OUTPUT = ('s3://org.unencrypted.emr.output/apache_sample/%s' %
+          datetime.datetime.utcnow().strftime("%s"))
+
+print """\
+Running pig job with settings:
+
+    SCRIPT={script}
+    INPUT={input}
+    OUPUT={output}
+""".format(script=pig_file, input=INPUT, output=OUTPUT)
 
 pig_args = ['-p', 'INPUT=%s' % INPUT,
             '-p', 'OUTPUT=%s' % OUTPUT]
@@ -22,14 +32,14 @@ pig_args = ['-p', 'INPUT=%s' % INPUT,
 pig_step = PigStep('Process Reports', pig_file, pig_args=pig_args)
 steps = [InstallPigStep(), pig_step]
 
-job_id = conn.run_jobflow(name='sample apache report',
-
-                          steps=steps,
-                          log_uri="s3://org.unencrypted.emr.log/sampleflow_logs",
-                          enable_debugging=True,
-                          ami_version="latest",
-                          hadoop_version="1.0.3",
-                          instance_groups=instance_groups,
-                          keep_alive=False)
+job_id = conn.run_jobflow(
+    name='sample apache report',
+    ec2_keyname=os.getenv("EC2_KEY_NAME"),
+    steps=steps,
+    log_uri="s3://org.unencrypted.emr.log/sampleflow_logs",
+    enable_debugging=True,
+    ami_version="latest",
+    instance_groups=instance_groups,
+    keep_alive=True)
 
 print job_id
